ed50746aba006a7a1efd4ca93c7239d8e56c515b
Tim Perry <tim.r.perry@gmail.com>
2016-04-18 21:48:42 +0100

    added some themes and disabled startup message

--- init.el
+++ init.el
@@ -1,7 +1,26 @@
 (load "~/.emacs.d/init/keys")
 (load "~/.emacs.d/init/packages")
 
+(defconst base-path (file-name-directory load-file-name))
+
 (require 'use-package)
 
 (use-package rainbow-delimiters)
-(use-package helm-config)
+(use-package helm
+  :init
+  (setq helm-M-x-fuzzy-match t
+      helm-bookmark-show-location t
+      helm-buffers-fuzzy-matching t
+      helm-completion-in-region-fuzzy-match t
+      helm-file-cache-fuzzy-match t
+      helm-imenu-fuzzy-match t
+      helm-mode-fuzzy-match t
+      helm-locate-fuzzy-match t
+      helm-quick-update t
+      helm-recentf-fuzzy-match t
+      helm-semantic-fuzzy-match t))
+
+(add-to-list 'custom-theme-load-path (concat base-path "/themes"))
+(load-theme 'base16-3024-dark t)
+
+(setq inhibit-startup-message t)
--- init/keys.el
+++ init/keys.el
@@ -4,6 +4,7 @@
 ; buffers
 (global-set-key (kbd "M-{") 'previous-buffer)
 (global-set-key (kbd "M-}") 'next-buffer)
+(global-set-key (kbd "s-k") '(lambda () (interactive) (kill-buffer (buffer-name))))
 
 ; mini buffer
 (global-set-key (kbd "M-x") 'helm-M-x)
