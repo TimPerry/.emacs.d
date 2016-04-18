38a347a8df1b5a2293d357fd4193cdc594dd477b
Tim Perry <tim.r.perry@gmail.com>
2016-04-18 22:03:24 +0100

    added more key mappings for typical navigation

--- init/keys.el
+++ init/keys.el
@@ -4,7 +4,16 @@
 ; buffers
 (global-set-key (kbd "M-{") 'previous-buffer)
 (global-set-key (kbd "M-}") 'next-buffer)
-(global-set-key (kbd "s-k") '(lambda () (interactive) (kill-buffer (buffer-name))))
+(global-set-key (kbd "M-k") '(lambda () (interactive) (kill-buffer (buffer-name))))
+(global-set-key (kbd "M-u") 'revert-buffer)
+(global-set-key (kbd "M-s") 'save-buffer)
+
+; nav
+(global-set-key (kbd "M-<left>") 'beginning-of-line)
+(global-set-key (kbd "M-<right>") 'end-of-line)
+
+(global-set-key (kbd "A-<left>") 'left-word)
+(global-set-key (kbd "A-<right>") 'right-word)
 
 ; mini buffer
 (global-set-key (kbd "M-x") 'helm-M-x)
