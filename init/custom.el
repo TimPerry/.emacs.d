27237fa74d3694dc6d2faa81afbd20aa648d2668
Tim Perry <tim.r.perry@gmail.com>
2016-04-19 22:37:26 +0100

    adding jpop custom setup

--- init/custom.el
+++ init/custom.el
@@ -1,3 +1,7 @@
 (custom-set-variables
+'(jpop-completion-func (quote car))
 '(jpop-project-directory "~/Projects")
+'(jpop-filter-regexps
+   (quote
+    ("~$" "\\.o$" "\\.exe$" "\\.a$" "/\\.svn" "\\.elc$" "\\.output$" "\\.$" "#$" "\\.class$" "\\.png$" "\\.svn*" "\\/node_modules\\/*" "\\.gif$" "\\.gem$" "\\.pdf$" "\\.swp$" "\\.iml$" "\\.jar$" "\\/build\\/" "/\\.git" "\\/jsdoc\\/" "\\.min\\.js$" "\\.tags$" "\\.filecache" "\\.cache$" "\\/.git\\/" "\\/report\\/" "\\.gcov\\.html$" "\\.func.*\\.html$" "\\/tmp\\/")))
 )
