19fce5a873c6d8c60aa33a931d0ffe2d8d218c70
Tim Perry <tim.r.perry@gmail.com>
2016-04-19 21:51:36 +0100

    adding web-mode

--- init.el
+++ init.el
@@ -46,6 +46,16 @@
 ;  :init
 ;  (powerline-default-theme))
 
+; modes
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
 ; reset
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
