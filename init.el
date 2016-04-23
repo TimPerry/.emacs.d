eb28f3b93fa337a7e8a5cc212791611f23895f71
Tim Perry <tim.r.perry@gmail.com>
2016-04-23 22:54:07 +0100

    Adding js2 mode to jsx files

--- init.el
+++ init.el
@@ -112,7 +112,8 @@
 (use-package js2-mode
   :ensure t
   :config
-    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+    (add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode)))
 
 (use-package jsx-mode
   :ensure t
