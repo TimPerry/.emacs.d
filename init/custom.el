fa7c2262ea0757bc291826b0ab76c840d529b424
Tim Perry <tim.r.perry@gmail.com>
2016-04-24 21:24:06 +0100

    Theming powerline

--- init/custom.el
+++ init/custom.el
@@ -3,20 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(powerline-color-alist (quote ((powerline-color1 1) (powerline-color2 1))) t)
- '(powerline-color0 "#282828")
- '(powerline-color1 "#3C3C3C" t)
- '(powerline-color2 "#515151" t)
- '(powerline-foreground "#eeeeec")
  '(jpop-completion-func (quote car))
  '(jpop-filter-regexps
    (quote
     ("~$" "\\.o$" "\\.exe$" "\\.a$" "/\\.svn" "\\.elc$" "\\.output$" "\\.$" "#$" "\\.class$" "\\.png$" "\\.svn*" "\\/node_modules\\/*" "\\.gif$" "\\.gem$" "\\.pdf$" "\\.swp$" "\\.iml$" "\\.jar$" "\\/build\\/" "/\\.git" "\\/jsdoc\\/" "\\.min\\.js$" "\\.tags$" "\\.filecache" "\\.cache$" "\\/.git\\/" "\\/report\\/" "\\.gcov\\.html$" "\\.func.*\\.html$" "\\/tmp\\/")))
  '(jpop-project-directory "~/Projects")
+ '(powerline-color-alist (quote ((powerline-color1 1) (powerline-color2 1))) t)
+ '(powerline-color0 "#282828")
+ '(powerline-color1 "#3C3C3C" t)
+ '(powerline-color2 "#515151" t)
+ '(powerline-foreground "#eeeeec")
+ '(powerline-height 30)
  '(shell-pop-universal-key "s-t"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(powerline-active1 ((t (:inherit mode-line :background "#282828"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "#3C3C3C"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#515151"))))
+ '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#eeeeec")))))
