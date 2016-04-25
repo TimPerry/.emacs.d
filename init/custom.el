3740bbcc3c35b67cc399686c8f7a5140ad2823f1
Tim Perry <tim.r.perry@gmail.com>
2016-04-25 20:42:30 +0100

    Adding timperry.co.uk theming to powerline

--- init/custom.el
+++ init/custom.el
@@ -10,10 +10,10 @@
     ("~$" "\\.o$" "\\.exe$" "\\.a$" "/\\.svn" "\\.elc$" "\\.output$" "\\.$" "#$" "\\.class$" "\\.png$" "\\.svn*" "\\/node_modules\\/*" "\\.gif$" "\\.gem$" "\\.pdf$" "\\.swp$" "\\.iml$" "\\.jar$" "\\/build\\/" "/\\.git" "\\/jsdoc\\/" "\\.min\\.js$" "\\.tags$" "\\.filecache" "\\.cache$" "\\/.git\\/" "\\/report\\/" "\\.gcov\\.html$" "\\.func.*\\.html$" "\\/tmp\\/")))
  '(jpop-project-directory "~/Projects")
  '(powerline-color-alist (quote ((powerline-color1 1) (powerline-color2 1))) t)
- '(powerline-color0 "#282828")
- '(powerline-color1 "#3C3C3C" t)
- '(powerline-color2 "#515151" t)
- '(powerline-foreground "#eeeeec")
+ '(powerline-color0 "#3C444C")
+ '(powerline-color1 "#96C348" t)
+ '(powerline-color2 "#1F2127" t)
+ '(powerline-foreground "#FFFFFF")
  '(powerline-height 30)
  '(shell-pop-universal-key "s-t"))
 (custom-set-faces
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(powerline-active1 ((t (:inherit mode-line :background "#282828"))))
- '(powerline-active2 ((t (:inherit mode-line :background "#3C3C3C"))))
- '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#515151"))))
- '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#eeeeec")))))
+ '(powerline-active1 ((t (:inherit mode-line :background "#3C444C"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "#96C348"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#1F2127"))))
+ '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#FFFFFF")))))
