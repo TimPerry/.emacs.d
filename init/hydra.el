fd9f24a00c1b523723cc88bb1af5ef3c3ef30479
Tim Perry <tim.perry@bbc.co.uk>
2016-06-12 17:56:42 +0100

    Fixing typo in hydra

--- init/hydra.el
+++ init/hydra.el
@@ -1,9 +1,9 @@
 (use-package hydra
   :bind ("s-k" . hydra-kill-buffers/body))
 
-(defhydra hydra-kill-buffers
+(defhydra hydra-kill-buffers (nil nil)
   "Kill Buffers"
-  ("a" kill-all-buffers "All")
-  ("c" kill-current-buffer "Current")
-  ("o" kill-other-buffers "Other")
+  ("a" kill-all-buffers "all")
+  ("c" kill-current-buffer "current")
+  ("o" kill-other-buffers "other")
   ("q" nil "quit"))
