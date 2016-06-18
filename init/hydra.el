65eeaf155c9c95060638bbbf810649c312439445
Tim Perry <tim.perry@bbc.co.uk>
2016-06-18 16:36:03 +0100

    Updating buffer hydra for next/prev nav

--- init/hydra.el
+++ init/hydra.el
@@ -2,8 +2,10 @@
   :bind ("s-k" . hydra-kill-buffers/body))
 
 (defhydra hydra-kill-buffers (nil nil)
-  "Kill Buffers"
-  ("a" kill-all-buffers "all")
-  ("c" kill-current-buffer "current")
-  ("o" kill-other-buffers "other")
+  "Manage Buffers"
+  ("a" kill-all-buffers "kill all")
+  ("k" kill-current-buffer "kill current")
+  ("o" kill-other-buffers "kill others")
+  ("n" next-buffer "next buffer")
+  ("p" previous-buffer "previous buffer")
   ("q" nil "quit"))
